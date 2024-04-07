aded9c94592f810171b2af6e60410b818536c8c7
Jack Clayton <jackos@me.com>
2024-04-07 12:32:54 -0700

    Updates

--- example.md
+++ example.md
@@ -3,6 +3,7 @@ Show me a very simple rust function
 ```
 
 Sure, here's an example of a simple Rust function. This function takes two integers as parameters and returns their sum:
+
 ```rust
 fn add(a: i32, b: i32) -> i32 {
     return a + b;
@@ -12,19 +13,19 @@ fn main() {
     println!("The sum is {}", result);
 }
 ```
+
 ```output
 The sum is 12
 ```
+
 ```mojo
 #file: cool.mojo
 
 print("wow sweet one")
 ```
+
 ```output
 wow sweet one
 ```
 
 This `add` function has a descriptive name, and it's clear from the parameter list and the return type that the function adds two i32 integers together.
-
-
-
--- src/languages/mojo.ts
+++ src/languages/mojo.ts
@@ -3,11 +3,11 @@ import { mkdirSync, writeFileSync } from "fs";
 import { getTempPath } from "../config";
 import { Cell, CommentDecorator } from "../types";
 import path from "path";
-import {window} from "vscode";
+import { window } from "vscode";
 
 let tempDir = getTempPath();
 
-export let processCellsMojo = (cells: Cell[]): {stream: ChildProcessWithoutNullStreams, clearOutput: boolean} => {
+export let processCellsMojo = (cells: Cell[]): { stream: ChildProcessWithoutNullStreams, clearOutput: boolean } => {
     let innerScope = "";
     let cellCount = 0;
     let clearOutput = false;
@@ -21,12 +21,12 @@ export let processCellsMojo = (cells: Cell[]): {stream: ChildProcessWithoutNullS
         let i = 0
         for (let line of lines) {
             i++
-            if (i==1 && line.replace(/\s/g, "").substring(0, 6) == "#file:") {
+            if (i == 1 && line.replace(/\s/g, "").substring(0, 6) == "#file:") {
                 let file = line.split(":")[1].trim()
-                if (file != "main.mojo"){
+                if (file != "main.mojo") {
                     let cleaned = ""
-                    for(let line2 of lines){
-                        if(line2.trim() != 'print("!!output-start-cell")'){
+                    for (let line2 of lines) {
+                        if (line2.trim() != 'print("!!output-start-cell")') {
                             cleaned += line2 + "\n"
                         }
                     }
@@ -44,9 +44,9 @@ export let processCellsMojo = (cells: Cell[]): {stream: ChildProcessWithoutNullS
             if (
                 line.startsWith("fn main():") ||
                 line.startsWith("def main():") ||
-                line.includes('Python.import_module("sys")') || 
+                line.includes('Python.import_module("sys")') ||
                 line.trim() == "from python import Python"
-            ){
+            ) {
                 continue;
             }
             if (line[0] !== " " && i == len && !line.includes("#") && line.trim().split(" ").length == 1 && !line.endsWith(")")) {
@@ -65,10 +65,10 @@ export let processCellsMojo = (cells: Cell[]): {stream: ChildProcessWithoutNullS
 
     let mainFile = path.join(tempDir, "main.mojo");
     const activeFilePath = path.dirname(window.activeTextEditor?.document.uri.fsPath as string);
-    let header = `def main():\n    from python import Python\n    let sys = Python.import_module("sys")\n    sys.path.append("${activeFilePath}")\n    sys.path.append("${tempDir}")\n`
+    let header = `def main():\n    from python import Python\n    sys = Python.import_module("sys")\n    sys.path.append("${activeFilePath}")\n    sys.path.append("${tempDir}")\n`
 
     mkdirSync(tempDir, { recursive: true });
     writeFileSync(mainFile, header + innerScope);
 
-    return {stream: spawn('mojo', [mainFile]), clearOutput};
+    return { stream: spawn('mojo', [mainFile], { env: process.env }), clearOutput };
 };
